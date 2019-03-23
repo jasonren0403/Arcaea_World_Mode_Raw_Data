f2dbc5838a1eb20f0ce888c9b5db1bab48f814e9
esterTion <lcz970@126.com>
2019-03-23 09:27:08 +0800

    cnwiki gen

--- gen.php
+++ gen.php
@@ -2,6 +2,7 @@
 chdir(__DIR__);
 
 $f= fopen('out.txt', 'w');
+$cn = fopen('out_cn.txt', 'w');
 
 $map = array(
   '0-0'=>'Fragments',
@@ -43,15 +44,28 @@ $charater = [
   'Hikari & Seine',
   'Saya'
 ];
+$charater_cn = [
+  0,1,2,3,4,5,'对立（Axium Crisis）','对立（Grievous Lady）',8,'光&菲希卡','依莉丝','爱托','露娜',12,'光（Zero）','光（Fracture）',
+  'Hikari (Summer)',
+  'Tairitsu (Summer)',
+  '对立 & 托凛',
+  '彩梦',
+  '爱托 & 露娜 -冬-',
+  '梦',
+  '光 & 晴音',
+  '咲弥'
+];
 // two files from unpacked game
 $songlist = json_decode(file_get_contents('songlist'), true);
 $packlist = json_decode(file_get_contents('packlist'), true);
-$songs = []; $packs = [];
+$songs = []; $packs = []; $packs_cn = [];
 foreach ($songlist['songs'] as &$song) {
   $songs[$song['id']] = $song['title_localized']['en'];
 }
 foreach ($packlist['packs'] as &$pack) {
   $packs[$pack['id']] = $pack['name_localized']['en'];
+  $packs_cn[$pack['id']] = $pack['name_localized']['en'];
+  if (isset($pack['custom_banner']) && $pack['id'] != 'base') $packs_cn[$pack['id']] .= ' Collaboration';
 }
 
 chdir(__DIR__);
@@ -66,6 +80,12 @@ foreach (glob('*-*.json') as $file) {
 {| style="width: 500px;" class="article-table mw-collapsible" cellspacing="1" cellpadding="1" border="0"
 |-
 ! scope="col"|Tile !! scope="col"|Step<br />(remaining to next reward) !! scope="col"|Special tile !! scope="col"|Reward
+');
+  fwrite($cn, '= '.$numid.' '.$id.' =
+
+{| class="wikitable mw-collapsible mw-collapsed" border="1" cellspacing="0" cellpadding="5" style="text-align:center"
+|-
+! scope="col"|级数 !! scope="col"|步数<br />（到下一奖励的步数） !! scope="col"|特殊阶梯 !! scope="col"|奖励
 ');
   $total = [
     'step' => 0,
@@ -98,19 +118,23 @@ foreach (glob('*-*.json') as $file) {
     }
     $remaining -= $step;
     $restriction = [];
+    $restriction_cn = [];
     if (isset($tile['step_type'])) {
       foreach ($tile['step_type'] as $st) {
         switch ($st) {
           case 'plusstamina': {
             $restriction[] = '+'.$tile['plus_stamina_value'].' stamina';
+            $restriction_cn[] = '+'.$tile['plus_stamina_value'].' 体力';
             break;
           }
           case 'randomsong': {
             $restriction[] = 'Random song';
+            $restriction_cn[] = '随机歌曲';
             break;
           }
           case 'speedlimit': {
             $restriction[] = 'Fixed Speed ≤ '.($tile['speed_limit_value']/10);
+            $restriction_cn[] = '限速 ≤ '.($tile['speed_limit_value']/10);
             break;
           }
           default: {
@@ -122,26 +146,53 @@ foreach (glob('*-*.json') as $file) {
     }
     if (isset($tile['restrict_type'])) {
       switch ($tile['restrict_type']) {
-        case 'song_id': $restriction[] = 'Restrict [['.$songs[$tile['restrict_id']].']]'; break;
-        case 'pack_id': $restriction[] = 'Restrict [['.$packs[$tile['restrict_id']].']]'; break;
+        case 'song_id': {$restriction[] = 'Restrict [['.$songs[$tile['restrict_id']].']]'; $restriction_cn[] = '限制 [['.$songs[$tile['restrict_id']].']]'; break;}
+        case 'pack_id': {$restriction[] = 'Restrict [['.$packs[$tile['restrict_id']].']]'; $restriction_cn[] = '限制 [['.$packs_cn[$tile['restrict_id']].']]'; break;}
       }
     }
     if (empty($restriction)) $restriction[] = '-';
+    if (empty($restriction_cn)) $restriction_cn[] = '-';
     $restriction = implode("<br />", $restriction);
+    $restriction_cn = implode("<br />", $restriction_cn);
     $reward = [];
+    $reward_cn = [];
     if (isset($tile['items'])) {
       if (isset($tile['items'][1])) {
         print_r($tile['items']);exit;
       }
       switch($tile['items'][0]['type']) {
-        case 'fragment': $reward[] = getFrag($tile['items'][0]) .' fragments'; $total['frag'] += getFrag($tile['items'][0]); break;
-        case 'character': $reward[] = '[['. $charater[$tile['items'][0]['id']].']]'; $total['song']++; break;
-        case 'world_song': $reward[] = '[['. $songs[$tile['items'][0]['id']].']]'; $total['char']++; break;
+        case 'fragment': {
+          $reward[] = getFrag($tile['items'][0]) .' fragments';
+          $reward_cn[] = getFrag($tile['items'][0]) .' 残片';
+          $total['frag'] += getFrag($tile['items'][0]); break;
+        }
+        case 'character': {
+          $reward[] = '[['. $charater[$tile['items'][0]['id']].']]';
+          $reward_cn[] = '[[搭档#|'. $charater_cn[$tile['items'][0]['id']].']]';
+          $total['song']++; break;
+        }
+        case 'world_song': {
+          $reward[] = '[['. $songs[$tile['items'][0]['id']].']]';
+          $reward_cn[] = '[['. $songs[$tile['items'][0]['id']].']]';
+          $total['char']++; break;
+        }
         case 'core': {
           switch ($tile['items'][0]['id']) {
-            case 'core_generic': { $reward[] = 'Ether Drop &times; '.$tile['items'][0]['amount']; $total['ether']+=$tile['items'][0]['amount']; break; }
-            case 'core_hollow': { $reward[] = 'Hollow Core &times; '.$tile['items'][0]['amount']; $total['hollow']+=$tile['items'][0]['amount']; break; }
-            case 'core_desolate': { $reward[] = 'Desolate Core &times; '.$tile['items'][0]['amount']; $total['desolate']+=$tile['items'][0]['amount']; break; }
+            case 'core_generic': {
+              $reward[] = 'Ether Drop &times; '.$tile['items'][0]['amount'];
+              $reward_cn[] = '以太之滴 &times; '.$tile['items'][0]['amount'];
+              $total['ether']+=$tile['items'][0]['amount']; break;
+            }
+            case 'core_hollow': {
+              $reward[] = 'Hollow Core &times; '.$tile['items'][0]['amount'];
+              $reward_cn[] = '中空核心 &times; '.$tile['items'][0]['amount'];
+              $total['hollow']+=$tile['items'][0]['amount']; break;
+            }
+            case 'core_desolate': {
+              $reward[] = 'Desolate Core &times; '.$tile['items'][0]['amount'];
+              $reward_cn[] = '荒芜核心 &times; '.$tile['items'][0]['amount'];
+              $total['desolate']+=$tile['items'][0]['amount']; break;
+            }
             default: {print_r($tile['items']);exit;}
           }
           break;
@@ -150,9 +201,13 @@ foreach (glob('*-*.json') as $file) {
       }
     }
     $reward = implode('<br />', $reward) ?: '-';
+    $reward_cn = implode('<br />', $reward_cn) ?: '-';
 
     fwrite($f, "|-
 | ${i} || ${step} (${remaining}) || ${restriction} || ${reward}
+");
+  fwrite($cn, "|-
+| ${i} || ${step} (${remaining}) || ${restriction_cn} || ${reward_cn}
 ");
   }
   $total_reward = [$total['frag'].' fragments'];
@@ -161,8 +216,21 @@ foreach (glob('*-*.json') as $file) {
   if ($total['ether']) $total_reward[] = $total['ether'].' Ether Drop'.($total['ether']>1?'s':'');
   if ($total['hollow']) $total_reward[] = $total['hollow'].' Hollow Core'.($total['hollow']>1?'s':'');
   if ($total['desolate']) $total_reward[] = $total['desolate'].' Desolate Core'.($total['desolate']>1?'s':'');
+  $total_reward_cn = [$total['frag'].' 残片'];
+  if ($total['song']) $total_reward_cn[] = $total['song'].' 角色';
+  if ($total['char']) $total_reward_cn[] = $total['char'].' 歌曲';
+  if ($total['ether']) $total_reward_cn[] = $total['ether'].' 以太之滴';
+  if ($total['hollow']) $total_reward_cn[] = $total['hollow'].' 中空核心';
+  if ($total['desolate']) $total_reward_cn[] = $total['desolate'].' 荒芜核心';
   fwrite($f, '|-
 | Total || \'\'\''.$total['step'].'\'\'\' || \'\'\'-\'\'\' || \'\'\''.implode(', ', $total_reward).'\'\'\'
+|}
+
+-------
+
+');
+  fwrite($cn, '|-
+| 总计 || \'\'\''.$total['step'].'\'\'\' || \'\'\'-\'\'\' || \'\'\''.implode('<br />', $total_reward_cn).'\'\'\'
 |}
 
 -------
